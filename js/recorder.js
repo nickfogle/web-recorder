bba1b2b8898ee988ba7da0731f303bb518eac91a
Nick Fogle <nickfogle@gmail.com>
2016-07-10 19:39:56 -0400

    Fix broken audio node connect method.

--- js/recorder.js
+++ js/recorder.js
@@ -5,24 +5,24 @@
   URL = window.URL || window.webkitURL;
 
   var audioContext = new AudioContext;
+
   var audioInLevel = audioContext.createGain();
   audioInLevel.gain.value = 0.5;
 
-  var source;
-  source.connect(audioInLevel);
-  audioInLevel.connect(audioContext.destination);
+  var audioIn = void 0;
 
   if (navigator.getUserMedia) {
     navigator.getUserMedia({ audio: true }, function(stream) {
-      source = audioContext.createMediaStreamSource(stream);
-    }, function(err) {
-     console.log('The following gUM error occured: ' + err);
+    audioIn = audioContext.createMediaStreamSource(stream);
+    audioIn.connect(audioInLevel);
+  }, function(err) {
+     console.log('The following error occured: ' + err);
    });
   } else {
      console.log('getUserMedia not supported on your browser!');
   }
 
-  recorder = new WebAudioRecorder(mixer, {
+  recorder = new WebAudioRecorder(audioInLevel, {
     workerDir: "bower_components/web-recorder/js/"
   });
 
@@ -32,8 +32,15 @@
     console.log('encoder loaded...');
   }
 
+  recorder.onComplete = function(recorder, blob) {
+    saveRecording(blob, recorder.encoding);
+  };
+
+  recorder.onError = function(recorder, message) {
+    console.log(message);
+  };
+
   $('#record').on('click', function() {
-    console.log('clicked');
     if (recorder.isRecording()) {
       stopRecording(true);
     } else {
@@ -42,16 +49,21 @@
   });
 
   $('#cancel').on('click', function() {
-     stopRecording(false);
-   });
+    stopRecording(false);
+  });
+
+  $('#recording-list').on('click', 'button', function(event) {
+    var url = $(event.target).attr('recording');
+    $("p[recording='" + url + "']").remove();
+    URL.revokeObjectURL(url);
+  });
 
   var startRecording = function() {
      $('#recording').removeClass('hidden');
      $('#record').html('STOP');
      $('#cancel').removeClass('hidden');
-     recorder.setOptions({timeLimit: 20, progressInterval: 1000, bufferSize: 1024, mp3: { bitRate: 320 }});
+     recorder.setOptions({timeLimit: 20, progressInterval: 1000, bufferSize: 1024, mp3: { bitRate: 160 }});
      recorder.startRecording();
-     setProgress(0);
   };
 
   var stopRecording = function(finish) {
@@ -65,9 +77,16 @@
     }
   };
 
-  recorder.onComplete = function(recorder, blob) {
-    console.log(blob);
-    saveRecording(blob, recorder.encoding);
+  var onGotAudioIn = function(stream) {
+    audioIn = audioContext.createMediaStreamSource(stream);
+    audioIn.connect(audioInLevel);
+  };
+
+  var saveRecording = function(blob, enc) {
+    var time = new Date();
+    var url = URL.createObjectURL(blob);
+    var html = "<p recording='" + url + "'>" + "<audio controls src='" + url + "'></audio> " + "(" + enc + ") " + (time.toString()) + " " + "<a class='btn btn-default' href='" + url + "' download='recording." + enc + "'>" + "Save..." + "</a> " + "<button class='btn btn-danger' recording=" + url + ">Delete</button>" + "</p>";
+    $('#recording-list').prepend($(html));
   };
 
   var minSecStr = function(n) {
@@ -75,36 +94,11 @@
   };
 
   var updateDateTime = function() {
-    var sec;
     $('#date-time').html((new Date).toString());
-    sec = recorder.recordingTime() | 0;
+    var sec = recorder.recordingTime() | 0;
     $('#time-display').html("" + (minSecStr(sec / 60 | 0)) + ":" + (minSecStr(sec % 60)));
   };
 
   window.setInterval(updateDateTime, 200);
-
-  var onGotAudioIn = function(stream) {
-    audioIn = audioContext.createMediaStreamSource(stream);
-    audioIn.connect(audioInLevel);
-  };
-
-  var saveRecording = function(blob, enc) {
-    var html, time, url;
-    time = new Date();
-    url = URL.createObjectURL(blob);
-    html = ("<p recording='" + url + "'>") + ("<audio controls src='" + url + "'></audio> ") + ("(" + enc + ") " + (time.toString()) + " ") + ("<a class='btn btn-default' href='" + url + "' download='recording." + enc + "'>") + "Save..." + "</a> " + ("<button class='btn btn-danger' recording='" + url + "'>Delete</button>");
-    "</p>";
-    $('#recording-list').prepend($(html));
-  };
-
-  $('#recording-list').on('click', 'button', function(event) {
-    var url = $(event.target).attr('recording');
-    $("p[recording='" + url + "']").remove();
-    URL.revokeObjectURL(url);
-  });
-
-  recorder.onError = function(recorder, message) {
-    console.log(message);
-  };
 
 }).call(this);
