3a7d8510eb5ebf0dd89e9cb4a07ab5e40c583e6b
Nick Fogle <nickfogle@gmail.com>
2016-08-11 02:10:03 -0400

    Update with audioContext checker.

--- recorder.js
+++ recorder.js
@@ -8,80 +8,89 @@ angular.module('wavveRecorder', [])
 
     URL = window.URL || window.webkitURL;
 
-    var audioContext = new AudioContext;
-    var audioInLevel = audioContext.createGain();
-    audioInLevel.gain.value = 0.5;
-    var audioIn = void 0;
+    var contextClass = (window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext);
 
-    var recorder = new WebAudioRecorder(audioInLevel, {
-      workerDir: "/bower_components/web-recorder/js/"
-    });
+    if (contextClass) {
+      console.log('Web Audio API is available.');
+      var audioContext = new contextClass();
+      var audioContext = new AudioContext;
+      var audioInLevel = audioContext.createGain();
+      audioInLevel.gain.value = 0.5;
+      var audioIn = void 0;
 
-    recorder.setEncoding("mp3");
+      var recorder = new WebAudioRecorder(audioInLevel, {
+        workerDir: "/bower_components/web-recorder/js/"
+      });
 
-    recorder.onEncoderLoading = function(recorder, encoding) {
-      console.log('encoder loaded...');
+      recorder.setEncoding("mp3");
+
+      recorder.onEncoderLoading = function(recorder, encoding) {
+        console.log('encoder loaded...');
+      }
+
+      recorder.onComplete = function(recorder, blob) {
+        saveRecording(blob, recorder.encoding);
+      };
+
+      recorder.onError = function(recorder, message) {
+        console.log(message);
+      };
+
+      var saveRecording = function(blob, enc) {
+        var blobObj = {};
+        blobObj.blob = blob;
+        blobObj.url = URL.createObjectURL(blob);
+        $rootScope.$broadcast("blobCreated", blobObj);
+      };
+
+      var startRecording = function() {
+         recorder.setOptions({timeLimit: 60, progressInterval: 1000, bufferSize: 1024, mp3: { bitRate: 160 }});
+         recorder.startRecording();
+      };
+
+      var stopRecording = function(finish) {
+        if (finish) {
+          recorder.finishRecording();
+        } else {
+          recorder.cancelRecording();
+        }
+      };
+
+      var onGotAudioIn = function(stream) {
+        audioIn = audioContext.createMediaStreamSource(stream);
+        audioIn.connect(audioInLevel);
+      };
+
+      var minSecStr = function(n) {
+        return (n < 10 ? "0" : "") + n;
+      };
+
+      var updateDateTime = function() {
+        var sec = recorder.recordingTime() | 0;
+        $('#time-display').html("" + (minSecStr(sec / 60 | 0)) + ":" + (minSecStr(sec % 60)));
+        // GET NASTY JQUERY OUT OF HERE ^^^
+      };
+
+      var loadRecorder = function() {
+        if (navigator.getUserMedia) {
+          navigator.getUserMedia({audio: true}, function(stream) {
+            audioIn = audioContext.createMediaStreamSource(stream);
+            audioIn.connect(audioInLevel);
+            audioIn.track = stream.getTracks()[0];
+            }, function(err) {
+              console.log('The following error occured: ' + err);
+            });
+        } else {
+          console.log('getUserMedia not supported on your browser!');
+        }
+        startRecording();
+        window.setInterval(updateDateTime, 200);
+      };
+
+
+    } else {
+      console.log('nope.')
     }
-
-    recorder.onComplete = function(recorder, blob) {
-      saveRecording(blob, recorder.encoding);
-    };
-
-    recorder.onError = function(recorder, message) {
-      console.log(message);
-    };
-
-    var saveRecording = function(blob, enc) {
-      var blobObj = {};
-      blobObj.blob = blob;
-      blobObj.url = URL.createObjectURL(blob);
-      $rootScope.$broadcast("blobCreated", blobObj);
-    };
-
-    var startRecording = function() {
-       recorder.setOptions({timeLimit: 60, progressInterval: 1000, bufferSize: 1024, mp3: { bitRate: 160 }});
-       recorder.startRecording();
-    };
-
-    var stopRecording = function(finish) {
-      if (finish) {
-        recorder.finishRecording();
-      } else {
-        recorder.cancelRecording();
-      }
-    };
-
-    var onGotAudioIn = function(stream) {
-      audioIn = audioContext.createMediaStreamSource(stream);
-      audioIn.connect(audioInLevel);
-    };
-
-    var minSecStr = function(n) {
-      return (n < 10 ? "0" : "") + n;
-    };
-
-    var updateDateTime = function() {
-      var sec = recorder.recordingTime() | 0;
-      $('#time-display').html("" + (minSecStr(sec / 60 | 0)) + ":" + (minSecStr(sec % 60)));
-      // GET NASTY JQUERY OUT OF HERE ^^^
-    };
-
-    var loadRecorder = function() {
-      if (navigator.getUserMedia) {
-        navigator.getUserMedia({audio: true}, function(stream) {
-          audioIn = audioContext.createMediaStreamSource(stream);
-          audioIn.connect(audioInLevel);
-          audioIn.track = stream.getTracks()[0];
-          }, function(err) {
-            console.log('The following error occured: ' + err);
-          });
-      } else {
-        console.log('getUserMedia not supported on your browser!');
-      }
-      startRecording();
-      window.setInterval(updateDateTime, 200);
-    };
-
 
     return {
 
